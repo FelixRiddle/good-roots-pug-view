eb679a7779652ae0c96db70a504c28386843d06a
Felix <fedecraft3312@gmail.com>
2023-08-30 19:16:14 -0300

    Learning tailwind, also made a formulary

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 const loginFormulary = (req, res) => {
     res.render("auth/login", {
-        
+        page: "Login"
     });
 }
 
 const registerFormulary = (req, res) => {
     res.render("auth/register", {
+        page: "Register"
     });
 }
 
