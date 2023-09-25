406200dad25759e2484f6f7eedb76241f7ba88a9
Felix <fedecraft3312@gmail.com>
2023-09-25 12:56:33 -0300

    Added some fields for the formulary

--- routes/user/userProperty.js
+++ routes/user/userProperty.js
@@ -2,10 +2,12 @@ import express from "express"
 
 import {
     admin,
+    create,
 } from "../../controllers/propertyController.js";
 
 const router = express.Router();
 
 router.get("/admin", admin);
+router.get("/create", create);
 
 export default router;
