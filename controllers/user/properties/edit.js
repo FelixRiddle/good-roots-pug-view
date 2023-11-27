34f25ee5666009727db21fb0fdfe220190071390
Felix <fedecraft3312@gmail.com>
2023-11-27 10:19:59 -0300

    Updated category and price selectable, it was pretty bad before

--- controllers/user/properties/edit.js
+++ controllers/user/properties/edit.js
@@ -1,4 +1,22 @@
+import Property from "../../../models/Property.js";
+
 const edit = async (req, res) => {
+    
+    const { id } = req.params;
+    
+    // Check that property exists
+    const property = Property.findByPk(id);
+    
+    if(!property) {
+        return res.redirect("/admin");
+    }
+    
+    // Check that person that accessed this url
+    // is the person that owns this property
+    if(property.userId.toString() !== req.user.id.toString()) {
+        return res.redirect("/admin");
+    }
+    
     // Get price and category
     const [
         categories,
