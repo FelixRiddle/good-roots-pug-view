42e0fe386117ca68cb8d036c7f3d1698fd58e65b
Felix <fedecraft3312@gmail.com>
2023-11-29 10:19:13 -0300

    fixed part of the problem

--- controllers/user/properties/admin.js
+++ controllers/user/properties/admin.js
@@ -10,7 +10,7 @@ const admin = async (req, res) => {
     console.log(`ID: ${id}`);
     
     // Fetch properties from the database that are owned by this user
-    const properties_res = await Property.findAll({
+    const propertiesRes = await Property.findAll({
         where: {
             userId: id,
         },
@@ -29,7 +29,7 @@ const admin = async (req, res) => {
     
     // Thanks sensei for this incredible response
     // https://stackoverflow.com/questions/64546830/sequelize-how-to-eager-load-with-associations-raw-true
-    const properties = properties_res.map(x => x.get({ plain: true }));
+    const properties = propertiesRes.map(x => x.get({ plain: true }));
     
     return res.render("user/property/admin", {
         page: "My Properties",
