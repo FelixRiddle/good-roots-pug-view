24214a4fe39fa8330752525e704f3cee1d215d4a
Felix <fedecraft3312@gmail.com>
2023-12-02 12:09:05 -0300

    Redoing the post request on property creation

--- src/public/js/routes/user/property/create.js
+++ src/public/js/routes/user/property/create.js
@@ -1,5 +1,4 @@
-import { body } from "express-validator";
-import formFetchAllValues from "../../../lib/form/formFetchAllValues.js";
+import { Validator, formFetchAllValues } from "felixriddle.checkpoint";
 
 // Get submit button
 let submitBtn = document.getElementById("createProperty");
@@ -9,7 +8,9 @@ if(!submitBtn) {
 }
 
 // Hook the click event
-submitBtn.onclick("click", (event) => {
+submitBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
+    
     console.log(`The user clicked the button.`);
     
     // Every input element name
@@ -30,64 +31,17 @@ submitBtn.onclick("click", (event) => {
     let resultObject = formFetchAllValues(inputElementsNames);
     if(resultObject) {
         console.log(`Fetch form values Ok`);
+        console.log(`Data: `, resultObject);
     } else {
         console.log(`Couldn't fetch form values!`);
     }
     
     // TODO: Frontend validation
-    
-    // body("title")
-    //     .notEmpty().withMessage("The title is required")
-    //     // Someone might be messing with endpoints so you always have to set a limit no matter what field is
-    //     .isLength({
-    //         max: 64
-    //     }).withMessage("Title can't be longer than 64 characters"),
-    // body("description")
-    //     .notEmpty().withMessage("Description is required")
-    //     .isLength({
-    //         max: 512,
-    //     }).withMessage("Description is too large"),
-    // body("category")
-    //     .isNumeric().withMessage("Select a category")
-    //     .notEmpty().withMessage(friendly_message)
-    //     .isLength({
-    //         max: 16
-    //     }).withMessage(friendly_message),
-    // body("price")
-    //     .isNumeric().withMessage("Select a price")
-    //     .notEmpty().withMessage(friendly_message)
-    //     .isLength({
-    //         max: 16
-    //     }).withMessage(friendly_message),
-    // // These are all numbers
-    // body("rooms")
-    //     .isNumeric().withMessage("Select rooms")
-    //     .notEmpty().withMessage(friendly_message)
-    //     .isLength({
-    //         max: 16
-    //     }).withMessage(friendly_message),
-    // body("parking")
-    //     .isNumeric().withMessage("Select parking spots quantity")
-    //     .notEmpty().withMessage(friendly_message)
-    //     .isLength({
-    //         max: 16
-    //     }).withMessage(friendly_message),
-    // body("bathrooms")
-    //     .isNumeric().withMessage("Select number of bathrooms")
-    //     .notEmpty().withMessage(friendly_message)
-    //     .isLength({
-    //         max: 16
-    //     }).withMessage(friendly_message),
-    // // Property location
-    // body("latitude")
-    //     .notEmpty().withMessage(property_location),
-    // body("longitude")
-    //     .notEmpty().withMessage(property_location),
-    // body("street")
-    //     .notEmpty().withMessage(property_location)
+    let validator = new Validator();
+    return;
     
     // Post data
-    axios.post('/create', {
+    await axios.post('/create', {
         property: resultObject,
     }).then(function (response) {
         console.log(response);
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,7 @@
 import path from "path";
 
 let jsPath = "./src/public/js/";
-
-/**
- * Get the list of files to compile into lib.js on the frontend
- * 
- * @returns {array}
- */
-function libFiles() {
-    // Forms path
-    let formsPath = `${jsPath}lib/form`;
-    let formFilesName = [
-        "formFetchAllValues",
-        "validation",
-        "validationResult"
-    ];
-    let formsSrcs = [];
-    for(let fileName of formFilesName) {
-        formsSrcs.push(`${formsPath}/${fileName}.js`);
-    }
-    console.log(`Compilation result: `, formsSrcs);
-    
-    return formsSrcs;
-}
+let publicRoutes = "public/js/routes/";
 
 export default {
     mode: "development",
@@ -30,18 +9,15 @@ export default {
         map: `${jsPath}map.js`,
         insertImage: `${jsPath}insertImage.js`,
         // Libraries
-        lib: {
-            import: libFiles(),
-        },
+        // lib: {
+        //     import: libFiles(),
+        // },
         // Comunication with routes
-        routes: {
+        create: {
             import: [
                 `${jsPath}routes/user/property/create.js`
             ],
-            // To what does it depends on
-            dependOn: [
-                "lib"
-            ]
+            filename: `routes/user/property/create.js`
         }
     },
     output: {
