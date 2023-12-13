330ede205869107ab4dee0b55a1777ee2b57711a
Felix <fedecraft3312@gmail.com>
2023-12-13 14:40:57 -0300

    Leaflet quickstart

--- src/routes/examples/map/quick_start.js
+++ src/routes/examples/map/quick_start.js
@@ -1,9 +1,14 @@
 import express from "express";
 
+import expand from "../../../controllers/expand.js";
+
 const quickStartRouter = express.Router();
 
 quickStartRouter.get("/quick_start", (req, res) => {
-    return res.render("examples/map/quick_start");
+    return res.render("examples/map/quick_start", {
+        page: "Quick start",
+        ...expand(req),
+    });
 });
 
 export default quickStartRouter;
