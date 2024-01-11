56999e0af2776931ffd138e027d530161cc1dbf4
Felix <fedecraft3312@gmail.com>
2024-01-11 10:54:34 -0300

    Full fledged registration tests

--- spec/routes/auth/register.spec.js
+++ spec/routes/auth/register.spec.js
@@ -4,24 +4,25 @@ import { serverUrl } from "../../../src/controllers/env/env.js";
 import TestAuthAPI from "../../../src/api/auth/TestAuthAPI.js";
 import { confirmUserEmail } from "./authUtils.js";
 
-describe("auth/register", () => {
+describe("User register", () => {
     // Setup dotenv
     dotenv.config({
         path: ".env"
     });
-    
-    // Create user data
-    const userData = {
-        name: "Some name",
-        email: "some_email@email.com",
-        password: "asd12345",
-        confirmPassword: "asd12345"
-    };
-    
     const url = serverUrl();
-    const api = new TestAuthAPI(userData, url);
     
-    it('Register user', async function() {
+    // Successful registration
+    it('Successful user registration', async function() {
+        // Create user data
+        const userData = {
+            name: "Some name",
+            email: "some_email@email.com",
+            password: "asd12345",
+            confirmPassword: "asd12345"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
         const registerRes = await api.registerUser();
         
         // Confirm user email
@@ -35,5 +36,153 @@ describe("auth/register", () => {
         
         // This is practically the same as jest
         expect(registerRes.userRegistered).toBe(true);
+    });
+    
+    // --- Password ---
+    // Short user password
+    it("Short user password on register", async function() {
+        // Create user data
+        const userData = {
+            name: "Some name",
+            email: "some_email3@gmail.com",
+            password: "asd",
+            confirmPassword: "asd"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
+        // The user will not be able to register so we can skip deletion
+        const registerRes = await api.registerUser();
+        const registered = registerRes.userRegistered;
+        
+        // If it's false, then the user is not registered
+        // Flip the bit to check against true
+        expect(!registered).toBe(true);
+    });
+    
+    // Longer than 64 characters password
+    it("Password too large", async function() {
+        // Create user data
+        const userData = {
+            name: "Some name",
+            email: "some_email4@gmail.com",
+            password: "Woh0dgvEByn6skV1BpUvx7X7XLio0HdaHtrMpacGBTCFImpjHUTb5fERCWkvV5A2A",
+            confirmPassword: "Woh0dgvEByn6skV1BpUvx7X7XLio0HdaHtrMpacGBTCFImpjHUTb5fERCWkvV5A2A"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
+        // The user will not be able to register so we can skip deletion
+        const registerRes = await api.registerUser();
+        const registered = registerRes.userRegistered;
+        
+        // If it's false, then the user is not registered
+        // Flip the bit to check against true
+        expect(!registered).toBe(true);
+    });
+    
+    // Confirm password mismatch
+    it("Password too large", async function() {
+        // Create user data
+        const userData = {
+            name: "Some name",
+            email: "some_email5@gmail.com",
+            password: "asdf1234",
+            confirmPassword: "asdf12345"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
+        // The user will not be able to register so we can skip deletion
+        const registerRes = await api.registerUser();
+        const registered = registerRes.userRegistered;
+        
+        // If it's false, then the user is not registered
+        // Flip the bit to check against true
+        expect(!registered).toBe(true);
+    });
+    
+    // --- Email ---
+    it("Bad email", async function() {
+        // Create user data
+        const userData = {
+            name: "Some name",
+            // Doesn't have domain name
+            email: "some_email6@.com",
+            password: "asdf1234",
+            confirmPassword: "asdf1234"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
+        // The user will not be able to register so we can skip deletion
+        const registerRes = await api.registerUser();
+        const registered = registerRes.userRegistered;
+        
+        // If it's false, then the user is not registered
+        // Flip the bit to check against true
+        expect(!registered).toBe(true);
+    });
+    
+    // --- Name ---
+    it("Null name", async function() {
+        // Create user data
+        const userData = {
+            name: "",
+            email: "some_email5@gmail.com",
+            password: "asdf1234",
+            confirmPassword: "asdf1234"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
+        // The user will not be able to register so we can skip deletion
+        const registerRes = await api.registerUser();
+        const registered = registerRes.userRegistered;
+        
+        // If it's false, then the user is not registered
+        // Flip the bit to check against true
+        expect(!registered).toBe(true);
+    });
+    
+    it("Name too short", async function() {
+        // Create user data
+        const userData = {
+            name: "aa",
+            email: "some_email5@gmail.com",
+            password: "asdf1234",
+            confirmPassword: "asdf1234"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
+        // The user will not be able to register so we can skip deletion
+        const registerRes = await api.registerUser();
+        const registered = registerRes.userRegistered;
+        
+        // If it's false, then the user is not registered
+        // Flip the bit to check against true
+        expect(!registered).toBe(true);
+    });
+    
+    it("Name too long", async function() {
+        // Create user data
+        const userData = {
+            // Name too long
+            name: "CsSAVM7muEYXrEkZd6n8T1SoPoxgMNxsZ2UJSlthE6BPSCmeX72jU5EHULhn7rq7rAUNYtGeTOeW7URRra4fQ5DNQVF0iMYv80wkbU9I7bv0T30rvTlLkJKTufo0FPgqA",
+            email: "some_email5@gmail.com",
+            password: "asdf1234",
+            confirmPassword: "asdf1234"
+        };
+        
+        const api = new TestAuthAPI(userData, url);
+        
+        // The user will not be able to register so we can skip deletion
+        const registerRes = await api.registerUser();
+        const registered = registerRes.userRegistered;
+        
+        // If it's false, then the user is not registered
+        // Flip the bit to check against true
+        expect(!registered).toBe(true);
     });
 });
