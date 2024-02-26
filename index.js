147240c2b0f0a3ad08d0ce78864a74387242127e
Felix <fedecraft3312@gmail.com>
2024-02-26 10:18:15 -0300

    Property api and model exports

---
+++ index.js
@@ -0,0 +1,13 @@
+import PropertyAPI from "./src/api/user/property/PropertyAPI.js";
+
+// Models
+import Model from "./src/models/index.js";
+
+export {
+    // Api's
+    PropertyAPI,
+    
+    // Models
+    // Every model
+    Model,
+};
---
+++ src/models/index.js
@@ -0,0 +1,15 @@
+import Category from "./Category.js";
+import DebugPropertyImageUpload from "./DebugPropertyImageUpload.js";
+import Price from "./Price.js";
+import Property from "./Property.js";
+import UserMessages from "./UserMessages.js";
+
+const MODEL = {
+    Category,
+    DebugPropertyImageUpload,
+    Price,
+    Property,
+    UserMessages,
+};
+
+export default MODEL;
