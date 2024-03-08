de6b92b44381ee90c95513b54fcb63295c272994
Felix <fedecraft3312@gmail.com>
2024-03-08 17:45:36 -0300

    Send user to frontend when visiting view property route

--- src/routes/property/view.js
+++ src/routes/property/view.js
@@ -1,31 +1,27 @@
 import express from "express";
 
-import {
-    Property,
-    Category,
-    Price,
-} from "app-models";
-
+import { PropertyModel, CategoryModel, PriceModel } from "../../mappings/models/index.js";
+import expand from "../../controllers/expand.js";
 import { serverUrl } from "../../controllers/env/env.js";
 
 const viewRoute = express.Router();
 
+/**
+ * View property by id
+ */
 viewRoute.get("/view/:id", async (req, res) => {
-    const home = `${serverUrl()}/home`;
-    const page404 = `${serverUrl()}/404`;
-    
     try {
         const { id } = req.params;
         
         // Fetch property
-        const property = await Property.findByPk(id, {
+        const property = await PropertyModel().findByPk(id, {
             include: [{
                 raw: true,
-                model: Category,
+                model: CategoryModel(),
                 as: "category"
             }, {
                 raw: true,
-                model: Price,
+                model: PriceModel(),
                 as: "price"
             }]
         });
@@ -33,13 +29,17 @@ viewRoute.get("/view/:id", async (req, res) => {
         // Check that the property exists
         if(!property) {
             console.log(`Property doesn't exists redirect to 404`);
+            const page404 = `${serverUrl()}/404`;
             return res.redirect(page404);
         }
         
         return res.render("property/view", {
             property,
+            ...expand(req)
         });
     } catch(err) {
+        const home = `${serverUrl()}/home`;
+        
         console.log(`Error when trying to view property`);
         console.error(err);
         console.log(`Redirect home`);
