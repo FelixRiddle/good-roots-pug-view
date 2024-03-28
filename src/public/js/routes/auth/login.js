13e42914261bc67fb4e8955a05498268e7744fce
Felix <fedecraft3312@gmail.com>
2024-03-28 12:46:33 -0300

    Fixed api to use the servers

--- src/public/js/routes/auth/login.js
+++ src/public/js/routes/auth/login.js
@@ -1,10 +1,8 @@
-import API from "felixriddle.good-roots-ts-api";
+import { AuthMarkupController } from "felixriddle.good-roots-ts-api";
 
 // Takes the login form and uploads data to the server
 (async() => {
     console.log(`Setting up a markup controller`);
-    const authMarkupCtrl = new API.AuthMarkupController();
+    const authMarkupCtrl = new AuthMarkupController();
     await authMarkupCtrl.loginPreset();
-    console.log(`Login preset`);
-    authMarkupCtrl.toggleRedirection();
 })();
